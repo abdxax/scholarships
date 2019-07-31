4af1fca99c80cdb12ce71fff66542152d4734288
unknown <abdxax@gmail.com>
2019-07-31 23:28:15 +0300

    first upload

---
+++ uhb/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css" integrity="sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If" crossorigin="anonymous">
+	<title></title>
+</head>
+<body >
+<header></header>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-10 offset-md-2">
+				<a href="" class="btn btn-info">xcxc</a>
+			</div>
+		</div>
+	</div>
+</section>
+</body>
+</html>
